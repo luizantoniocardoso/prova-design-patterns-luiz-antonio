4560f7cf8c929570e615e620998577268feda20a
Luiz Antonio De Souza Cardoso <luiz.129922@alunosatc.edu.br>
2025-10-29 19:31:33 -0300

    terminando a prova

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,51 @@
-console.log("Hello, World! 001");
+class Component {
+    showDetails() {
+      throw new Error("Método showDetails() deve ser implementado.");
+    }
+  }
+  
+  class Task extends Component {
+    constructor(name) {
+      super();
+      this.name = name;
+    }
+  
+    showDetails(indent = 0) {
+      console.log(`${" ".repeat(indent)}- Tarefa: ${this.name}`);
+    }
+  }
+  
+  class Project extends Component {
+    constructor(name) {
+      super();
+      this.name = name;
+      this.children = []; 
+    }
+  
+    add(component) {
+      this.children.push(component);
+    }
+  
+    remove(component) {
+      this.children = this.children.filter(c => c !== component);
+    }
+  
+    showDetails(indent = 0) {
+      console.log(`${" ".repeat(indent)}Projeto: ${this.name}`);
+      this.children.forEach(child => child.showDetails(indent + 2));
+    }
+  }
+  
+  const t1 = new Task("Escrever documentação");
+  const t2 = new Task("Fazer testes");
+  const subProject = new Project("Preparar lançamento");
+  subProject.add(new Task("Criar instalador"));
+  subProject.add(new Task("Gerar changelog"));
+  
+  const mainProject = new Project("Lançamento v1.0");
+  mainProject.add(t1);
+  mainProject.add(t2);
+  mainProject.add(subProject);
+  
+  mainProject.showDetails();
+  
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,42 @@
-console.log("Hello, World! 002");
+class LegacyPaymentSystem {
+    makePayment(amount) {
+      console.log(`Pagando R$${amount} com sistema legado.`);
+    }
+  }
+  
+  class ModernPaymentAPI {
+    process(amountInCents) {
+      console.log(`Pagamento de R$${amountInCents / 100} via API moderna.`);
+    }
+  }
+  
+  class ModernPaymentAdapter {
+    constructor(modernAPI) {
+      this.modernAPI = modernAPI;
+    }
+  
+    makePayment(amount) {
+      const amountInCents = amount * 100;
+      this.modernAPI.process(amountInCents);
+    }
+  }
+  
+  class PaymentProcessor {
+    constructor(system) {
+      this.system = system;
+    }
+  
+    pay(amount) {
+      this.system.makePayment(amount);
+    }
+  }
+  
+  const legacySystem = new LegacyPaymentSystem();
+  const processorLegacy = new PaymentProcessor(legacySystem);
+  processorLegacy.pay(100);
+  
+  const modernAPI = new ModernPaymentAPI();
+  const adaptedModernAPI = new ModernPaymentAdapter(modernAPI);
+  const processorModern = new PaymentProcessor(adaptedModernAPI);
+  processorModern.pay(250);
+  
